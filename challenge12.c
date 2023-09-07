763511124ea96abbb7a22db37ddc35c06a8e9a61
Sudenazkaranfil <114330977+Sudenazkaranfil@users.noreply.github.com>
2023-09-07 21:18:27 +0300

    Update challenge12.c

--- challenge12.c
+++ challenge12.c
@@ -43,8 +43,3 @@ int main()
     printf("Ciftlikte %d tane tavuk, %d tane koyun vardir.\n", tavukSayisi, koyunSayisi);
     return 0;
 }
-
-
-/* 💪🏻Hard: Bir yüzme havuzunda 2 adet su girişi, 1 adet su çıkışı vardır. İlk su girişi havuzu 10 saatte doldururken,
-ikinci su girişi havuzu 15 saatte doldurmaktadır. Havuzun kendiliğinden boşalma hızı ise 30 saatte bir doludur. Eğer havuz boşken,
-her iki su girişi de açılırsa havuz ne kadar sürede dolar?😀 */
